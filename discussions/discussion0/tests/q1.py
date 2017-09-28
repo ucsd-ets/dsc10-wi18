7f2ebcb86399e6151cbf9164f1eaff06d5be4bf2
Patrick Hayes <patrickghhayes@gmail.com>
2017-09-28 16:11:06 -0700

    Tests

---
+++ discussions/discussion0/tests/q1.py
@@ -0,0 +1,25 @@
+test = {
+  'name': 'Question 1_',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> 1 <= characters_q1 <= 5
+          True
+          >>> 1 <= characters_q2 <= 5
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
+
