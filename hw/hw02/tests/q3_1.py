72d596e21e79ed1842c5c41aad67779137f4fcb9
cmoyes <cmoyes.ucsd.edu>
2017-10-09 17:32:38 +0000

    hw02 added

---
+++ hw/hw02/tests/q3_1.py
@@ -0,0 +1,23 @@
+test = {
+  'name': 'Question 3_1',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> import numpy
+          >>> isinstance(second_element, numpy.int64)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
