ec624f2d4600dd5b1d288de236c8d587e2f10678
Siwei Xu <six019@ucsd.edu>
2017-10-12 18:14:41 -0700

    project 1 added

---
+++ projects/proj1/tests/q21.py
@@ -0,0 +1,29 @@
+test = {
+  'name': 'Question',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> per_capita_usage.sort(0).relabeled(1, 'V').take([0, 1, 2])
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> per_capita_usage.num_rows != 0
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
