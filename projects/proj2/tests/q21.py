f526391c44e413afc31409f6a39daf82035a12d7
Siwei Xu <tuyoikaze@gmail.com>
2017-11-03 14:39:35 +0900

    Project 2 uploaded

---
+++ projects/proj2/tests/q21.py
@@ -0,0 +1,21 @@
+test = {
+  'name': 'Question 2.1',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> simulate_game_optimal(10, 5) in range(11)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
