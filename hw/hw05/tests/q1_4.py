54c73b88a4d9cbe94e2131eec5147e02dcaa7d45
cmoyes <cmoyes.ucsd.edu>
2017-11-06 02:03:01 +0000

    hw05 added

---
+++ hw/hw05/tests/q1_4.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 0_4',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> isinstance(final_sample_size, int)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ hw/hw05/tests/q3_8.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 1_9',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> type(winnings) == np.float64
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
