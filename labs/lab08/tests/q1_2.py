29ccf14773a350377871d5d6c2add42aa2b5ec0f
Walker Carlson <wcarlson@ucsd.edu>
2017-11-19 12:55:38 -0800

    lab08 out

---
+++ labs/lab08/tests/q1_2.py
@@ -0,0 +1,22 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> q1_2 == [1, 2, 3]
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
