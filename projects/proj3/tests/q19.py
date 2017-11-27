b4f26f397d225083679a845b0205e7d46ccb8845
Siwei Xu <tuyoikaze@gmail.com>
2017-11-26 23:01:43 -0800

    Upload project 3

---
+++ projects/proj3/tests/q19.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> len(full_rob_data.column('Best Line')) > 0
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
