b5e69d2072eb19a606ad2f5e462ed6336dcce5ba
Patrick Hayes <patrickghhayes@gmail.com>
2018-01-13 18:55:49 -0800

    lab02

---
+++ Labs/lab02/tests/q51.py
@@ -0,0 +1,26 @@
+test = {
+  'name': 'Question',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> initial
+          2766000
+          >>> changed
+          2814000
+          >>> 0.017 < growth_rate < 0.018
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ Labs/lab02/tests/q53.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> 3.177 < fifty_year_growth_rate < 3.178
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
