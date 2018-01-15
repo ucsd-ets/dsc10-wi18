34c96de20f6d01bc9bc3335f48e494c42dbe8d0f
Patrick Hayes <patrickghhayes@gmail.com>
2018-01-14 21:09:26 -0800

    update variable name lab02

--- Labs/lab02/tests/q53.py
+++ Labs/lab02/tests/q53.py
@@ -6,7 +6,18 @@ test = {
       'cases': [
         {
           'code': r"""
-          >>> 3.177 < fifty_year_growth_rate < 3.178
+          >>> try:
+          ...     fifteen_year_growth_rate
+          ... except NameError:
+          ...     pass
+          ... else:
+          ...     print((3.177 < fifteen_year_growth_rate < 3.178) or (116.39 < fifteen_year_growth_rate < 116.40))
+          ... try:
+          ...     fifty_year_growth_rate
+          ... except NameError:
+          ...     pass
+          ... else:
+          ...     print((3.177 < fifty_year_growth_rate < 3.178) or (116.39 < fifty_year_growth_rate < 116.40))
           True
           """,
           'hidden': False,
